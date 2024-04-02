a44c12851156cf3cc4dd3e459d0c5ea97a0af65a
Jéssica O Silva <jessyolie@gmail.com>
2024-04-01 22:27:46 -0300

    #feature/02_FUNDAMENTOS - Renderização

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,6 +5,14 @@ const Events = () => {
         console.log("Ativou o evento");
     };
 
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Renderizando isso!</h1>;
+        } else {
+            return <h1>Também posso renderizar isso!</h1>;
+        }
+    }
+
     return (
         <div>
             <div>
@@ -21,6 +29,8 @@ const Events = () => {
                 }}
                 >Clique aqui, por favor</button>
             </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     );
 };
