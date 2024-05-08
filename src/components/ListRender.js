ebb6309e6b35e90e68c2cadd7cb5a10fe8871b1f
Jéssica O Silva <jessyolie@gmail.com>
2024-05-07 21:48:28 -0300

    #3_AVANCANDO_EM_REACT - Listas com key

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -3,11 +3,22 @@ import { useState } from "react";
 const ListRender = () => {
     const [list] = useState(["Jessica", "Ana", "Cre"]);
 
+    const [users] = useState([
+        {id:1, nome:"Jessica", age:25},
+        {id:2, nome:"Ana", age:15},
+        {id:3, nome:"Cre", age:50}
+    ]);
+
     return (
         <div>
             <ul>
                 {list.map((item) => 
                     <li>{item}</li>
+                )}
+            </ul>
+            <ul>
+                {users.map((item) => 
+                    <li key={item.id}>{item.nome} - {item.age}</li>
                 )}
             </ul>
         </div>
